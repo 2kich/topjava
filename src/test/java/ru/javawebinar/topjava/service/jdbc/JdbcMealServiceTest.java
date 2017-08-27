8fef8588fcae786fb154d19cd8faf6ef888c0f09
Andrei Tuka <2kich11@gmail.com>
2017-08-27 22:22:32 +0300

    profiles_and_separate_tests

---
+++ src/test/java/ru/javawebinar/topjava/service/jdbc/JdbcMealServiceTest.java
@@ -0,0 +1,9 @@
+package ru.javawebinar.topjava.service.jdbc;
+
+import org.springframework.test.context.ActiveProfiles;
+
+import ru.javawebinar.topjava.service.AbstractMealServiceTest;
+
+@ActiveProfiles("JDBC")
+public class JdbcMealServiceTest extends AbstractMealServiceTest {
+}
