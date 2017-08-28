f1c9eca7420290a0824e4256c378fd65763ec5e6
Andrei Tuka <2kich11@gmail.com>
2017-08-28 20:06:24 +0300

    without_DataJpaMealServiceTest

--- src/test/java/ru/javawebinar/topjava/service/datajpa/DataJpaMealServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/datajpa/DataJpaMealServiceTest.java
@@ -1,8 +1,6 @@
 package ru.javawebinar.topjava.service.datajpa;
 
-
 import org.springframework.test.context.ActiveProfiles;
-
 import ru.javawebinar.topjava.service.AbstractMealServiceTest;
 
 import static ru.javawebinar.topjava.Profiles.DATAJPA;
